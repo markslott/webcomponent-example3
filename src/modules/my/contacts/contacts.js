52e91cc6981478c6d0ccd608253506a2dbd9dc87
Mark Lott <mlott@mlott-ltm5.internal.salesforce.com>
2020-04-07 18:06:51 -0500

    second pass complete

--- src/modules/my/contacts/contacts.js
+++ src/modules/my/contacts/contacts.js
@@ -2,8 +2,8 @@ import { LightningElement, api } from 'lwc';
 import fetchDataHelper from './fetchDataHelper.js';
 
 export default class Contacts extends LightningElement {
-
     @api title;
+    data = [];
 
     //in connectedCallback we retrieve data end render it
     connectedCallback() {
@@ -15,38 +15,8 @@ export default class Contacts extends LightningElement {
     }
 
     async getData() {
-        const data = await fetchDataHelper({amountOfRecords : 10});
+        const data = await fetchDataHelper({ amountOfRecords: 10 });
         this.data = data;
         console.log(data);
-        this.renderTable();
     }
-
-    renderTable() {
-        var table = `
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Website</th>
-                <th>Amount</th>
-                <th>Phone</th>
-            </tr>
-        </thead>
-        <tbody>`;
-        this.data.forEach((item) => {
-           let row = "<tr>";
-           row += "<td>"+item.name+"</td>";
-           row += "<td>"+item.email+"</td>";
-           row += "<td>"+item.website+"</td>";
-           row += "<td>"+item.amount+"</td>";
-           row += "<td>"+item.phone+"</td>";
-           row += "</tr>";
-           table += row;
-        });
-        table += "</tbody>";
-        // eslint-disable-next-line @lwc/lwc/no-inner-html
-        this.template.querySelector('.datatable').innerHTML = table;
-    }
-
-
 }
